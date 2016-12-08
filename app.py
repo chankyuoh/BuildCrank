d9c4de0c598c49ec2462ea1fddb2e61681eacdae
chankyu oh <chankyuoh@u.northwestern.edu>
2016-12-08 17:59:44 +0900

    hi

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import requests
 from flask import Flask, request
 app = Flask(__name__)
 
-with open('champData.json', 'r') as fp:
-    data = json.load(fp)
+
 
 
 @app.route('/', methods=['GET'])
@@ -39,6 +38,9 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
+
+                    with open('champData.json', 'r') as fp:
+                        data = json.load(fp)
 
                     res = ""
                     res += "Frequent Starter: " + str(data["Akali"]["FreqStarterBuild"]) + "\n"
