a640f184df85e17044eb85b6bab58c1fe1c5f9de
chankyu oh <chankyuoh@u.northwestern.edu>
2016-12-16 14:58:37 +0900

    fix Sticker bug

--- app.py
+++ app.py
@@ -35,7 +35,11 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    if "text" not in messaging_event["message"]:
+                        send_message(sender_id,"I only accept text-based communications :(")
+                        return "ok", 200
+                    else:
+                        message_text = messaging_event["message"]["text"]  # the message's text
                     sendAppropriateMessage(message_text,sender_id)
 
 
