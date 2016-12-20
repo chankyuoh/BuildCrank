551e959b8fe65a1ba214ff2efaac8d173f7af523
chankyu oh <chankyuoh@u.northwestern.edu>
2016-12-20 15:09:14 +0900

    add welcome message try 2

--- app.py
+++ app.py
@@ -11,11 +11,13 @@ app = Flask(__name__)
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
+
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
+    send_welcome_message()
     return "We can do whatever we want with BuildCrank", 200
 
 
